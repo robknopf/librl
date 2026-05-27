b510b32fb802d65404f9684ba5397fe43b177339
Rob Knopf <rknopf@whirlinggizmo.com>
2026-05-26 20:49:21 -0400

    Add handleKind to JS binding TypeScript declarations.

--- bindings/js/src/types.ts
+++ bindings/js/src/types.ts
@@ -400,6 +400,7 @@ export interface RLApi {
   initAsync(opts?: RLInitOptions): number;
   refreshScratch(): void;
   getTime(): number;
+  handleKind(handle: RLHandle): number;
   deinit(): Promise<void>;
   isInitialized(): boolean;
   getPlatform(): string;
